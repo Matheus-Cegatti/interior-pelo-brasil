484fd0f5a7d645f8ba6f68606b1abfefbbcf2d6f
Matheus-Cegatti <cegattimatheus@gmail.com>
2022-08-24 13:47:29 -0300

    fazendo o carrossel com opacity

--- script/main.js
+++ script/main.js
@@ -1,6 +1,36 @@
-const btnMenu = document.querySelector("#icone-principal")
-const menu = document.querySelector(".menu")
+let contador = 0;
+const imagens = document.querySelectorAll(".imagem");
+const maxImagens = imagens.length;
 
-btnMenu.addEventListener("click", () => {
-    menu.classList.toggle("menu-desativado")
-})
+
+
+
+function proxImagem() {
+
+    imagens[contador].classList.remove("imagem-atual");
+
+    contador++
+
+    if (contador >= maxImagens) {
+        contador = 0
+    }
+
+    imagens[contador].classList.add("imagem-atual");
+
+
+    // imagens[contador]
+    // console.log(contador);
+    //  console.log(imagens[contador]);
+
+
+
+
+}
+
+    setInterval(function() {
+        
+        proxImagem()
+    }, 2000)
+
+
+
