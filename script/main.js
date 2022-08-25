6409706adc512167cdce7b1e4ece214d50861b33
Matheus-Cegatti <cegattimatheus@gmail.com>
2022-08-25 14:08:12 -0300

    colocando preenchimento automatico do CEP-Cidade-UF

--- script/main.js
+++ script/main.js
@@ -1,3 +1,23 @@
+const preencheCadastro = (endereco) => {
+    document.getElementById("cidade").value = endereco.localidade;  
+    document.getElementById("estado-escolha").value = endereco.uf;
+}
+
+const buscaCep = async() => {
+    const cep = document.querySelector(".input-cep").value;
+    const url = `http://viacep.com.br/ws/${cep}/json/`;
+
+    const dados = await fetch(url);
+    const endereco = await dados.json();
+    preencheCadastro(endereco)
+}
+
+document.querySelector(".input-cep").addEventListener("focusout", buscaCep);
+
+
+
+
+
 let contador = 0;
 const imagens = document.querySelectorAll(".imagem");
 const maxImagens = imagens.length;
